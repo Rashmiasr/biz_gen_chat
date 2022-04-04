29d3f39b32ef090c61d2e38651d39937a9942237
Dhruv <dhruvsakariya2304@gmail.com>
2022-04-04 16:01:35 +0530

    new style

--- index.php
+++ index.php
@@ -26,7 +26,6 @@ function loginForm(){
     '<div id="loginform">
     <p>Please enter your name to continue!</p>
     <form action="index.php" method="post">
-      <label for="name">Name &mdash;</label>
       <input type="text" name="name" id="name" />
       <input type="submit" name="enter" id="enter" value="Enter" />
     </form>
