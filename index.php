6ac94fd93519bbc07521d1c8eec601a59c619efe
Rashmiasr <88921179+Rashmiasr@users.noreply.github.com>
2022-04-05 16:43:42 +0530

    Update index.php

--- index.php
+++ index.php
@@ -73,6 +73,7 @@ function loginForm(){
             </form>
         
     </div>
+     jQuery.noConflict();
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script type="text/javascript">
             // jQuery Document
