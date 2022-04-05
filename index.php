ae42b71a401dc21b3f20cdb5e4c4b2367826c81b
Rashmiasr <88921179+Rashmiasr@users.noreply.github.com>
2022-04-05 18:22:38 +0530

    Update index.php

--- index.php
+++ index.php
@@ -67,7 +67,7 @@ function loginForm(){
             ?>
             </div>
  
-            <form name="message" action="">
+            <form name="message" action="post">
                 <input name="usermsg" type="text" id="usermsg" />
                 <input name="submitmsg" type="submit" id="submitmsg" value="Send" />
             </form>
