7e42659c9362f8a4202ffca95c41d83aa5a450f0
Rashmiasr <88921179+Rashmiasr@users.noreply.github.com>
2022-04-05 20:48:02 +0530

    Update index.php

--- index.php
+++ index.php
@@ -79,7 +79,7 @@ function loginForm(){
             $(document).ready(function () {
                 $("#submitmsg").click(function () {
                     var clientmsg = $("#usermsg").val();
-                  //  $.post("post.php", { text: clientmsg });
+                    $.post("post.php", { text: clientmsg });
                     $("#usermsg").val("");
                     return false;
                 });
@@ -116,14 +116,4 @@ function loginForm(){
 </html>
 <?php
 }
-     
-//session_start();
-
-if(isset($_POST['usermsg'])){
-   $usermsg = $_POST['usermsg'];
-   
-    $text_message = "<div class='msgln'><span class='chat-time'>".date("g:i A")."</span> <b class='user-name'>".$_SESSION['name']."</b> ".stripslashes(htmlspecialchars($text))."<br></div>";
-    file_put_contents("log.html", $text_message, FILE_APPEND | LOCK_EX);
-}
-
 ?>
