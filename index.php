6ae71c014f2ef7ee5d3b9c694c509daf5366683e
Rashmiasr <88921179+Rashmiasr@users.noreply.github.com>
2022-04-05 22:36:16 +0530

    Update index.php

--- index.php
+++ index.php
@@ -74,7 +74,10 @@ function loginForm(){
         
     </div>
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        <script type="text/javascript">
+      <script>
+$.noConflict();
+// Code that uses other library's $ can follow here.
+ 
             // jQuery Document
             $(document).ready(function () {
                 $("#submitmsg").click(function () {
