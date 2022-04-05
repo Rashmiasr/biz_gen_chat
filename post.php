56318f4c4e3940c890dd5127ab2c485391bb6822
Rashmiasr <88921179+Rashmiasr@users.noreply.github.com>
2022-04-05 15:11:33 +0530

    Update post.php

--- post.php
+++ post.php
@@ -1,6 +1,10 @@
 <?php
 session_start();
 if(isset($_SESSION['name'])){
+    /*cookies*/
+ $cookieParams = session_get_cookie_params();
+    $cookieParams[samesite] = "None";
+    session_set_cookie_params($cookieParams);
     $text = $_POST['text'];
      
     $text_message = "<div class='msgln'><span class='chat-time'>".date("g:i A")."</span> <b class='user-name'>".$_SESSION['name']."</b> ".stripslashes(htmlspecialchars($text))."<br></div>";
