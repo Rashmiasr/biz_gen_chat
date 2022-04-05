1921c8a6c25683cd342f285f342c392d883edb4e
Rashmiasr <88921179+Rashmiasr@users.noreply.github.com>
2022-04-05 17:12:20 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
  
 session_start();
- 
+ $cookieParams = session_get_cookie_params();
+    $cookieParams[samesite] = "None";
+    session_set_cookie_params($cookieParams);
 if(isset($_GET['logout'])){    
      
     //Simple exit message
