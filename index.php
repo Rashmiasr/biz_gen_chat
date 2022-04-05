46e1a674b15b6d31263ed15ba102369ffd5a851a
Rashmiasr <88921179+Rashmiasr@users.noreply.github.com>
2022-04-05 21:18:36 +0530

    Update index.php

--- index.php
+++ index.php
@@ -116,8 +116,8 @@ function loginForm(){
 </html>
 <?php
 }
-     if(isset($_POST['usermsg'])){
-   $text = $_POST['usermsg'];
+     if(isset($_POST['submitmsg'])){
+   $text = $_POST['text'];
    
     $text_message = "<div class='msgln'><span class='chat-time'>".date("g:i A")."</span> <b class='user-name'>".$_SESSION['name']."</b> ".stripslashes(htmlspecialchars($text))."<br></div>";
     file_put_contents("log.html", $text_message, FILE_APPEND | LOCK_EX);
