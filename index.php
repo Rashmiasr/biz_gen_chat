d27e4931843a03041a638b08809db0a162b3a937
Rashmiasr <88921179+Rashmiasr@users.noreply.github.com>
2022-04-05 16:03:06 +0530

    Update index.php

--- index.php
+++ index.php
@@ -113,6 +113,7 @@ function loginForm(){
                 });
             });
         </script>
+      <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     </body>
 </html>
 <?php
