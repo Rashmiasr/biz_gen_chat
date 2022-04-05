80b874419468d01c3be35fd1fccda27a5ef6745a
Rashmiasr <88921179+Rashmiasr@users.noreply.github.com>
2022-04-05 17:15:33 +0530

    Update post.php

--- post.php
+++ post.php
@@ -1,5 +1,8 @@
 <?php
 session_start();
+$cookieParams = session_get_cookie_params();
+    $cookieParams[samesite] = "None";
+    session_set_cookie_params($cookieParams);
 if(isset($_SESSION['name'])){
     $text = $_POST['text'];
      
