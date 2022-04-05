da9db0bcc358e277fe2af9d33f653d7da8b75131
Rashmiasr <88921179+Rashmiasr@users.noreply.github.com>
2022-04-05 16:24:11 +0530

    Update index.php

--- index.php
+++ index.php
@@ -76,11 +76,8 @@ function loginForm(){
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script type="text/javascript">
             // jQuery Document
-          jQuery.noConflict();
             $(document).ready(function () {
-           
                 $("#submitmsg").click(function () {
-                  
                     var clientmsg = $("#usermsg").val();
                     $.post("post.php", { text: clientmsg });
                     $("#usermsg").val("");
