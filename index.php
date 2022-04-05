9d4676ec0580b06b416796715ee2455914f898d9
Rashmiasr <88921179+Rashmiasr@users.noreply.github.com>
2022-04-05 18:02:27 +0530

    Update index.php

--- index.php
+++ index.php
@@ -74,7 +74,7 @@ function loginForm(){
         
     </div>
     
-        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+     
      
         <script type="text/javascript">
             // jQuery Document
